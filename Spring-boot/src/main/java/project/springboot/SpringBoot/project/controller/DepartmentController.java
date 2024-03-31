afadfecc2d3b6af9143380a2af8d4c9ee67ccb7a
devamjariwala <jariwala.dev@northeastern.edu>
2024-03-31 14:25:02 -0400

    added logger logic in the department controller class

--- Spring-boot/src/main/java/project/springboot/SpringBoot/project/controller/DepartmentController.java
+++ Spring-boot/src/main/java/project/springboot/SpringBoot/project/controller/DepartmentController.java
@@ -1,6 +1,8 @@
 package project.springboot.SpringBoot.project.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import project.springboot.SpringBoot.project.entity.Department;
@@ -16,36 +18,44 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    //slf4j logging library, we don't have to include this in the pom.xml file, it is already available in the system.
+    private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        logger.info("Inside the saveDepartment method of DepartmentController Class");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        logger.info("Inside the fetchDepartmentList method of DepartmentController Class");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentByID(@PathVariable("id") Long departmentId){
+        logger.info("Inside the fetchDepartmentByID method of DepartmentController Class");
         return departmentService.fetchDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        logger.info("Inside the deleteDepartmentById method of DepartmentController Class");
         departmentService.deleteDepartmentById(departmentId);
         return "Department Deleted Successfully";
     }
 
     @PutMapping("/departments/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        logger.info("Inside the updateDepartment method of DepartmentController Class");
         return departmentService.updateDepartment(departmentId, department);
     }
 
     @GetMapping("/departments/name/{name}")
     public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        logger.info("Inside the fetchDepartmentByName method of DepartmentController Class");
         return departmentService.fetchDepartmentByName(departmentName);
-
-
     }
 }
